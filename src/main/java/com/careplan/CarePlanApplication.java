b4fbe35426047c386f4174beb9287fad0596433c
Yuqi <yuqishi95@gmail.com>
2026-03-01 00:59:59 -0800

    Day 1: Spring Boot project init + design doc

---
+++ src/main/java/com/careplan/CarePlanApplication.java
@@ -0,0 +1,12 @@
+package com.careplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarePlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarePlanApplication.class, args);
+    }
+}
